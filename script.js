6e2b52396c95f846391be987458d810741056995
jesusckantillo <jesuscantillocode@gmail.com>
2023-01-11 13:26:42 -0500

    Javascript implementation, open to improvements

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-let numberList = document.getElementsByClassName("numbers");
-let submitButton = document.getElementById("main_button");
-let ratingState = document.getElementsByClassName("rating_state");
-let thankState = document.getElementsByClassName("thanks_state");
-const numberValue = document.getElementById("number_value");
-let number;
+let numbers = document.querySelectorAll("li");
+let ratingState = document.querySelector(".rating_state");
+let sumbitButton = document.getElementById("main_button");
+let userRate = document.getElementById("user_rate");
+let thankState = document.querySelector(".thanks_state");
+let saveNumber;
 
 
-submitButton.addEventListener("click",changeDiv);
+sumbitButton.addEventListener("click",changeDiv);
+
+
+for(let i=0; i<numbers.length;i++){
+    numbers[i].addEventListener("click",changeColor);
+}
+
+function changeColor(event){
+    saveNumber = event.target.textContent;
+    event.target.style.backgroundColor = "hsl(25, 97%, 53%)"; 
+}
 
 function changeDiv(){
-    ratingState.style.display ="none";
+    ratingState.style.display = "none";
+    userRate.textContent = saveNumber;
+    thankState.style.display="flex";
 }
